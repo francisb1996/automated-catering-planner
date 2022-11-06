10ed7d20eb8ac6ef9a17edee80f0a38f824c20f1
francisb1996 <francisb1996@gmail.com>
2022-11-06 12:46:22 -0600

    test pdf, readme updates, debug config, read pdf and write text

--- README.md
+++ README.md
@@ -7,8 +7,14 @@ This script is designed to process a catering order PDF and export a detailed pl
 3. After install completes, run `python --version` and `pip --version` to make sure the install was successful
     * If commands fail, try restarting your computer
 
+## VSCode setup for Python
+1. Download the `Python` Extension 
+
 ## Run script
 `python process.py <input file path>`
 
 ## Debugging
-TODO
+A VSCode configuration is included in this project. Place breakpoints and run the `Process` configuration in the `Run and Debug` tab.
+
+## Output
+Output files are written to the `output/` directory
--- process.py
+++ process.py
@@ -1,1 +1,30 @@
-print('hello, world!')
+import sys
+from datetime import datetime
+from PyPDF2 import PdfReader
+
+path = sys.argv[1]
+
+print('running script...') 
+print('input path: ', path)
+print('parsing...')
+reader = PdfReader(path)
+text = ''
+for page in reader.pages:
+    text += page.extract_text() + '\n'
+print('parsed')
+
+print('processing...')
+result = text # Add processing logic
+print('processed')
+
+print('writing output...')
+timestamp = datetime.now().strftime('%d-%m-%y_%H-%M')
+inputFileName = path.split('/').pop().split('.')[0]
+outputFileName = f'output\\{inputFileName}_{timestamp}.txt'
+print('output path: ', outputFileName)
+outputFile = open(outputFileName, 'x')
+outputFile.write(result)
+outputFile.close()
+print('output complete')
+
+print('process completet')
